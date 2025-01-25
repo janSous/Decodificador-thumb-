e015390bca695b59286e401339f685dd9dce6580
Janison Sousa <guimaraesp323@gmail.com>
2025-01-25 20:33:37 -0300

    Atualizando função nome_reg

--- src/decodificador_reg.cpp
+++ src/decodificador_reg.cpp
@@ -2,6 +2,8 @@
 #include "decodificador_reg.h"
 using std::cout;
 
+
+
 std::string decoder_reg(int reg)
 {
     switch(reg)
@@ -82,7 +84,7 @@ void nome_reg(int num, int type)
         case TYPE_1: // Ld = #imediato << Lm| Ld = Lm >> #imediato
             reg = num & 0x7; //Bits 2-0(Ld)
             cout << decoder_reg(reg) << ", ";
-            reg = num & 0x78; //Pega os bits 5-3(Lm)
+            reg = num & 0x38; //Pega os bits 5-3(Lm)
             cout << decoder_reg(reg) << ", ";
             imed = num & 0x7C0; // Bits 10-6(immed5)
             cout << '#' << imed;
@@ -91,7 +93,7 @@ void nome_reg(int num, int type)
         case TYPE_1_1: // Ld, [Lm, #immed]
             reg = num & 0x7; //Bits 2-0(Ld)
             cout << decoder_reg(reg) << ", ";
-            reg = num & 0x78; // Bits 5-3(Lm)
+            reg = num & 0x38; // Bits 5-3(Lm)
             cout << "[ " << decoder_reg(reg);
             imed = num & 0x7C0; //Bits 10-6(immed5)
             cout << ", #" << imed << "]";
@@ -100,7 +102,7 @@ void nome_reg(int num, int type)
         case TYPE_2: // Ld, Lm, Ln
             reg = num & 0x7; //Ld(bits 2-0)
             cout << decoder_reg(reg) << ", ";
-            reg = num & 0x78; //Ln(bits 5-3)
+            reg = num & 0x38; //Ln(bits 5-3)
             cout << decoder_reg(reg) << ", ";
             reg = num & 0x1C0; // Lm(bits 8-5)
             cout << decoder_reg(reg);
@@ -109,7 +111,7 @@ void nome_reg(int num, int type)
         case TYPE_3: // Ld, Ln, immed
             reg = num & 0x7; //Ld(bits 2-0)
             cout << decoder_reg(reg) << ", ";
-            reg = num & 0x78;// Ln(bits 5-3)
+            reg = num & 0x38;// Ln(bits 5-3)
             cout << decoder_reg(reg) << ", #";
             imed = num & 0x1C0; // immed3(bits 8-6)
             cout << imed;
@@ -122,13 +124,104 @@ void nome_reg(int num, int type)
             cout << imed;
         break;
 
+        case TYPE_4_1: //Ld, [pc, #immed*4]
+            reg = num & 0x700; //Ld(bits 10-8)
+            cout << decoder_reg(reg) << ", [" << 
+            decoder_reg(15) << ", #";
+            imed = num & 0xFF; //immed8(bits 7-0)
+            cout << imed << "]";
+        break;
+
+        case TYPE_4_2: //Ld, [sp, #immed*4]
+            reg = num & 0x700; //Ld(bits 10-8)
+            cout << decoder_reg(reg) << ", [" <<
+            decoder_reg(14) << ", #";
+            imed = num & 0xFF; //immed8(bits 7-0)
+            cout << imed << "]";
+
         case TYPE_5: // Ld|Ln|Hd & 7|Hn & 7, Lm|Ls|Hm & 7
-            reg = num & 0x7; //Ld|Ln|Hd & 7
-            cout << decoder_reg(reg) << ", "
+            reg = num & 0x7; //Ld|Ln|Hd&7 (bits 2-0)
+            cout << decoder_reg(reg) << ", ";
+            reg = num & 0x38; // Lm|Ls|Hm&7 (bits 5-3)
+            cout << decoder_reg(reg);
+        break;
 
+        case TYPE_6_1: // PUSH|POP
+            reg = num & 0x1FF; //Conj. de registradores + pc/lr (bits 8-0)
+            int cont = 0;
 
+            if(reg != 0)
+            {
+                 cout << " {";
 
+            if(reg & 0x1 == 1)
+                cout << decoder_reg(0);
+                cont ++;
+            if(reg & 0x2 == 1)
+                aux(cont);
+                cout << decoder_reg(1);
+                cont++;
+            if(reg & 0x4 == 1)
+                aux(cont);
+                cout << decoder_reg(2);
+                cont++;
+            if(reg & 0x8 == 1)
+                aux(cont);
+                cout << decoder_reg(3);
+                cont++;
+            if(reg & 0x10 == 1)
+                aux(cont);
+                cout << decoder_reg(4);
+                cont++;
+            if(reg & 0x20 == 1)
+                aux(cont);
+                cout << decoder_reg(5);
+                cont++;
+            if(reg & 0x40 == 1)
+                aux(cont);
+                cout << decoder_reg(6);
+                cont++;
+            if(reg & 0x80 == 1)
+                aux(cont);
+                cout << decoder_reg(7);
+                cont++;
+            if(reg & 0x100 == 1)
+                aux(cont);
+                cout << decoder_reg(14);
+            if(reg & 0x100 == 0)
+                aux(cont);
+                cout << decoder_reg(15);
+            
+            cout << "}";
+            }
+        break;
+
+        case TYPE_7: // BKPT #immed8
+            imed = num & 0xFF; // immed8(bits 7-0)
+            cout << " #" << imed;
+        break;
         
+        case TYPE_8: //B #adress
+            imed = num & 0x7F; //offset com sinal(10-0)
+            imed = imed*2 + 4;
+            cout << " #" << imed;
+         // case TYPE_9:
+
+        case TYPE_10: //BL #adress
+            imed = num & 0x7FF; //offset com ou sem sinal (bits 11-0)
+            imed = imed*2 + 4;
+            cout << " #" << imed;
+        break;
+
+
 
     }
 }
+
+//Função auxiliar
+void aux(int cont)
+{
+    if(cont != 0)
+        cout << ", ";
+}
+
