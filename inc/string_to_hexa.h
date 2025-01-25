67309973e263115f291a006b43673f6a485d41a4
Janison Sousa <guimaraesp323@gmail.com>
2025-01-25 20:41:28 -0300

    Atualizando

---
+++ inc/string_to_hexa.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef HEXA_H
+#define HEXA_H
+
+void validar(){
+    
+int instruction = 0;
+int opcode_num; 
+int option;
+int num; 
+char *hexa;
+int j = 0; 
+int count = 0; 
+int a; 
+int valido = 0; 
+
+}
+
+
+
+
+
+
+#endif 
